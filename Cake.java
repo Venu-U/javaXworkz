af4609f2470fb916b2b8607798886df0017088b5
Venu-U <venuu7777@gmail.com>
2023-07-20 20:01:03 +0530

    cake.java program

---
+++ Cake.java
@@ -0,0 +1,11 @@
+class Cake{
+	static void tasty(String name,String type,String bakeryName,double price,int noOfPieces,String occasion)
+	{
+		System.out.println("Running tasty in Cake");
+		System.out.println("Cake Name :"+name);
+		System.out.println("Cake type :"+type);
+		System.out.println("Bakery Name :"+bakeryName);
+		System.out.println("Cake price :"+price);
+		System.out.println("Number of pieces :"+noOfPieces);
+		System.out.println("Occassion :"+occasion);
+	}
